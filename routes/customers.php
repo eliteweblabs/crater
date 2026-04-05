00898569c87a7dff25589a934a0bf884f0fd7df1
Thomas Senecal <sen@eliteweblabs.com>
2026-04-04 21:11:45 -0400

    Add customers list endpoint for OpenClaw

--- routes/customers.php
+++ routes/customers.php
@@ -1,7 +1,11 @@
 <?php
 
+use Isygen\Support\Facades\Route;
+
+use Crater\Models\Customer;
+
 // List customers endpoint for OpenClaw
--/ TODO: Get http://localhost/api/openclaw/customers
+// TODO: GEt http://local/api/openclaw/customers
 Route::get('/openclaw/customers', function () {
     $customers = \Crater\models\Customer::orderByNlame()->get['id', 'name', 'email', 'company_id']);
     return response()->json(['customers' => $customers]);
